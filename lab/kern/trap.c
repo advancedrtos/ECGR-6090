2752d0093958ddcb131f14f7a2eb4a2fd00aacc4
advancedrtos <advanced.RTOS@gmail.com>
2016-10-24 12:58:03 -0400

    Lab 3 done and Lab 4 files updated

--- lab/kern/trap.c
+++ lab/kern/trap.c
@@ -8,6 +8,11 @@
 #include <kern/monitor.h>
 #include <kern/env.h>
 #include <kern/syscall.h>
+#include <kern/sched.h>
+#include <kern/kclock.h>
+#include <kern/picirq.h>
+#include <kern/cpu.h>
+#include <kern/spinlock.h>
 
 static struct Taskstate ts;
 
@@ -55,6 +60,8 @@ static const char *trapname(int trapno)
 		return excnames[trapno];
 	if (trapno == T_SYSCALL)
 		return "System call";
+	if (trapno >= IRQ_OFFSET && trapno < IRQ_OFFSET + 16)
+		return "Hardware Interrupt";
 	return "(unknown trap)";
 }
 
@@ -84,28 +91,28 @@ trap_init(void)
 	void alignment_check();
 	void machine_check();
 	void simd_floating_point_error();
-	int sys_call();
+	void system_call();
 	
 	
-	SETGATE(idt[T_DIVIDE],0,GD_KT,divide_error,0);
-	SETGATE(idt[T_DEBUG], 0, GD_KT, debug_exception, 0);
-	SETGATE(idt[T_NMI], 0, GD_KT, non_maskable_interrupt, 0);
-	SETGATE(idt[T_BRKPT], 0, GD_KT, break_point, 3);
-	SETGATE(idt[T_OFLOW], 0, GD_KT, over_flow, 0);
-	SETGATE(idt[T_BOUND], 0, GD_KT, bounds_check, 0);
-	SETGATE(idt[T_ILLOP], 0, GD_KT, illegal_opcode, 0);
-	SETGATE(idt[T_DEVICE], 0, GD_KT, device_not_available, 0);
-	SETGATE(idt[T_DBLFLT], 0, GD_KT, double_fault, 0);
-	SETGATE(idt[T_TSS], 0, GD_KT, task_segment_switch, 0);
-	SETGATE(idt[T_SEGNP], 0, GD_KT, segment_not_present, 0);
-	SETGATE(idt[T_STACK], 0, GD_KT, stack_exception, 0);
-	SETGATE(idt[T_GPFLT], 0, GD_KT, general_protection_fault, 0);
-	SETGATE(idt[T_PGFLT], 0, GD_KT, page_fault, 0);
-	SETGATE(idt[T_FPERR], 0, GD_KT, floating_point_error, 0);
-	SETGATE(idt[T_ALIGN], 0, GD_KT, alignment_check , 0);
-	SETGATE(idt[T_MCHK], 0, GD_KT, machine_check, 0);
-	SETGATE(idt[T_SIMDERR], 0, GD_KT, simd_floating_point_error, 0);
-	SETGATE(idt[T_SYSCALL], 0, GD_KT, sys_call, 3);
+	SETGATE(idt[T_DIVIDE],1,GD_KT,divide_error,0);
+	SETGATE(idt[T_DEBUG], 1, GD_KT, debug_exception, 0);
+	SETGATE(idt[T_NMI], 1, GD_KT, non_maskable_interrupt, 0);
+	SETGATE(idt[T_BRKPT], 1, GD_KT, break_point, 3);
+	SETGATE(idt[T_OFLOW], 1, GD_KT, over_flow, 0);
+	SETGATE(idt[T_BOUND], 1, GD_KT, bounds_check, 0);
+	SETGATE(idt[T_ILLOP], 1, GD_KT, illegal_opcode, 0);
+	SETGATE(idt[T_DEVICE], 1, GD_KT, device_not_available, 0);
+	SETGATE(idt[T_DBLFLT], 1, GD_KT, double_fault, 0);
+	SETGATE(idt[T_TSS], 1, GD_KT, task_segment_switch, 0);
+	SETGATE(idt[T_SEGNP], 1, GD_KT, segment_not_present, 0);
+	SETGATE(idt[T_STACK], 1, GD_KT, stack_exception, 0);
+	SETGATE(idt[T_GPFLT], 1, GD_KT, general_protection_fault, 0);
+	SETGATE(idt[T_PGFLT], 1, GD_KT, page_fault, 0);
+	SETGATE(idt[T_FPERR], 1, GD_KT, floating_point_error, 0);
+	SETGATE(idt[T_ALIGN], 1, GD_KT, alignment_check , 0);
+	SETGATE(idt[T_MCHK], 1, GD_KT, machine_check, 0);
+	SETGATE(idt[T_SIMDERR], 1, GD_KT, simd_floating_point_error, 0);
+	SETGATE(idt[T_SYSCALL], 1 , GD_KT, system_call, 3);
 	
 	// Per-CPU setup 
 	trap_init_percpu();
@@ -115,6 +122,29 @@ trap_init(void)
 void
 trap_init_percpu(void)
 {
+	// The example code here sets up the Task State Segment (TSS) and
+	// the TSS descriptor for CPU 0. But it is incorrect if we are
+	// running on other CPUs because each CPU has its own kernel stack.
+	// Fix the code so that it works for all CPUs.
+	//
+	// Hints:
+	//   - The macro "thiscpu" always refers to the current CPU's
+	//     struct CpuInfo;
+	//   - The ID of the current CPU is given by cpunum() or
+	//     thiscpu->cpu_id;
+	//   - Use "thiscpu->cpu_ts" as the TSS for the current CPU,
+	//     rather than the global "ts" variable;
+	//   - Use gdt[(GD_TSS0 >> 3) + i] for CPU i's TSS descriptor;
+	//   - You mapped the per-CPU kernel stacks in mem_init_mp()
+	//
+	// ltr sets a 'busy' flag in the TSS selector, so if you
+	// accidentally load the same TSS on more than one CPU, you'll
+	// get a triple fault.  If you set up an individual CPU's TSS
+	// wrong, you may not get a fault until you try to return from
+	// user space on that CPU.
+	//
+	// LAB 4: Your code here:
+
 	// Setup a TSS so that we get the right stack
 	// when we trap to the kernel.
 	ts.ts_esp0 = KSTACKTOP;
@@ -136,7 +166,7 @@ trap_init_percpu(void)
 void
 print_trapframe(struct Trapframe *tf)
 {
-	cprintf("TRAP frame at %p\n", tf);
+	cprintf("TRAP frame at %p from CPU %d\n", tf, cpunum());
 	print_regs(&tf->tf_regs);
 	cprintf("  es   0x----%04x\n", tf->tf_es);
 	cprintf("  ds   0x----%04x\n", tf->tf_ds);
@@ -182,24 +212,40 @@ print_regs(struct PushRegs *regs)
 static void
 trap_dispatch(struct Trapframe *tf)
 {
-	int32_t ret;
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
-	switch (tf->tf_trapno){
-		case T_PGFLT:
-			page_fault_handler(tf);
-			break;
-		case T_BRKPT:
-			monitor(tf);
-			break;
-		case T_SYSCALL:
-			ret = syscall(tf->tf_regs.reg_eax, tf->tf_regs.reg_edx, tf->tf_regs.reg_ecx, tf->tf_regs.reg_ebx,
-				tf->tf_regs.reg_edi, tf->tf_regs.reg_esi);
-			tf->tf_regs.reg_eax = ret;
-			return;
-			break;
+
+
+	// Handle spurious interrupts
+	// The hardware sometimes raises these because of noise on the
+	// IRQ line or other reasons. We don't care.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_SPURIOUS) {
+		cprintf("Spurious interrupt on irq 7\n");
+		print_trapframe(tf);
+		return;
 	}
-	
+
+	// Handle clock interrupts. Don't forget to acknowledge the
+	// interrupt using lapic_eoi() before calling the scheduler!
+	// LAB 4: Your code here.
+
+	if (tf->tf_trapno == T_PGFLT) {
+		cprintf("PAGE FAULT\n");
+		page_fault_handler(tf);
+		return;
+	}
+	if (tf->tf_trapno == T_BRKPT) {
+		cprintf("BREAK POINT\n");
+		monitor(tf);
+		return;
+	}
+	if (tf->tf_trapno == T_SYSCALL) {
+		cprintf("SYSTEM CALL\n");
+		tf->tf_regs.reg_eax = 
+			syscall(tf->tf_regs.reg_eax, tf->tf_regs.reg_edx, tf->tf_regs.reg_ecx,
+				tf->tf_regs.reg_ebx, tf->tf_regs.reg_edi, tf->tf_regs.reg_esi);
+		return;
+	}
 	
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
@@ -210,6 +256,7 @@ trap_dispatch(struct Trapframe *tf)
 		return;
 	}
 	
+	
 }
 
 void
@@ -219,16 +266,33 @@ trap(struct Trapframe *tf)
 	// of GCC rely on DF being clear
 	asm volatile("cld" ::: "cc");
 
+	// Halt the CPU if some other CPU has called panic()
+	extern char *panicstr;
+	if (panicstr)
+		asm volatile("hlt");
+
+	// Re-acqurie the big kernel lock if we were halted in
+	// sched_yield()
+	if (xchg(&thiscpu->cpu_status, CPU_STARTED) == CPU_HALTED)
+		lock_kernel();
 	// Check that interrupts are disabled.  If this assertion
 	// fails, DO NOT be tempted to fix it by inserting a "cli" in
 	// the interrupt path.
 	assert(!(read_eflags() & FL_IF));
 
-	cprintf("Incoming TRAP frame at %p\n", tf);
-
 	if ((tf->tf_cs & 3) == 3) {
 		// Trapped from user mode.
+		// Acquire the big kernel lock before doing any
+		// serious kernel work.
+		// LAB 4: Your code here.
 		assert(curenv);
+
+		// Garbage collect if current enviroment is a zombie
+		if (curenv->env_status == ENV_DYING) {
+			env_free(curenv);
+			curenv = NULL;
+			sched_yield();
+		}
 
 		// Copy trap frame (which is currently on the stack)
 		// into 'curenv->env_tf', so that running the environment
@@ -245,9 +309,13 @@ trap(struct Trapframe *tf)
 	// Dispatch based on what type of trap occurred
 	trap_dispatch(tf);
 
-	// Return to the current environment, which should be running.
-	assert(curenv && curenv->env_status == ENV_RUNNING);
-	env_run(curenv);
+	// If we made it to this point, then no other environment was
+	// scheduled, so we should return to the current environment
+	// if doing so makes sense.
+	if (curenv && curenv->env_status == ENV_RUNNING)
+		env_run(curenv);
+	else
+		sched_yield();
 }
 
 
@@ -262,9 +330,42 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 
 	// LAB 3: Your code here.
+	if (tf->tf_cs== GD_KT)
+		panic("page_fault_handler: Page Fault in Kernel");
+
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
+
+	// Call the environment's page fault upcall, if one exists.  Set up a
+	// page fault stack frame on the user exception stack (below
+	// UXSTACKTOP), then branch to curenv->env_pgfault_upcall.
+	//
+	// The page fault upcall might cause another page fault, in which case
+	// we branch to the page fault upcall recursively, pushing another
+	// page fault stack frame on top of the user exception stack.
+	//
+	// The trap handler needs one word of scratch space at the top of the
+	// trap-time stack in order to return.  In the non-recursive case, we
+	// don't have to worry about this because the top of the regular user
+	// stack is free.  In the recursive case, this means we have to leave
+	// an extra word between the current top of the exception stack and
+	// the new stack frame because the exception stack _is_ the trap-time
+	// stack.
+	//
+	// If there's no page fault upcall, the environment didn't allocate a
+	// page for its exception stack or can't write to it, or the exception
+	// stack overflows, then destroy the environment that caused the fault.
+	// Note that the grade script assumes you will first check for the page
+	// fault upcall and print the "user fault va" message below if there is
+	// none.  The remaining three checks can be combined into a single test.
+	//
+	// Hints:
+	//   user_mem_assert() and env_run() are useful here.
+	//   To change what the user environment runs, modify 'curenv->env_tf'
+	//   (the 'tf' variable points at 'curenv->env_tf').
+
+	// LAB 4: Your code here.
 
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
