2982d9eb21e30649f11614c73ec68b2d85cbdf7f
advancedrtos <advanced.RTOS@gmail.com>
2016-11-16 21:11:48 -0500

    lab 4 done

--- lab/lib/ipc.c
+++ lab/lib/ipc.c
@@ -23,8 +23,37 @@ int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
 	// LAB 4: Your code here.
-	panic("ipc_recv not implemented");
-	return 0;
+//	panic("ipc_recv not implemented");
+//	return 0;
+	int r;
+	if(pg != NULL)
+	{
+		r = sys_ipc_recv(pg);
+	}
+	else
+	{
+		r = sys_ipc_recv((void * )0xF0000000);
+	}
+	if(r != 0 )
+	{
+		if(from_env_store != NULL)
+		{
+			*from_env_store = 0;
+			if(perm_store != NULL)
+				*perm_store = 0;
+		}
+		return r;
+	}
+	if(from_env_store != NULL)
+	{
+		*from_env_store = thisenv->env_ipc_from;
+		if(perm_store != NULL)
+			*perm_store = thisenv->env_ipc_perm;
+		
+	}
+	int value = thisenv->env_ipc_value;
+	
+	return value;
 }
 
 // Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
@@ -39,7 +68,29 @@ void
 ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
 {
 	// LAB 4: Your code here.
-	panic("ipc_send not implemented");
+	//panic("ipc_send not implemented");
+	int r = 100;
+	if(pg == NULL)
+	{
+		while(r != 0)
+		{
+			r = sys_ipc_try_send(to_env, val, (void *)0xF0000000, 0);	
+			if(r != -E_IPC_NOT_RECV && r !=0)
+				panic("The destination environment is not receiving. Error:%e\n",r);
+			sys_yield();
+		}
+		
+	}
+	else
+	{
+		while(r != 0)
+		{
+			r = sys_ipc_try_send(to_env, val, pg, perm);	
+			if(r != -E_IPC_NOT_RECV && r !=0)
+				panic("The destination environment is not receiving. Error:%e\n",r);
+			sys_yield();
+		}
+	}
 }
 
 // Find the first environment of the given type.  We'll use this to
